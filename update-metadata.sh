02f73255b1954661b6a6a7f6a3ca676033d7dd49
shurik204 <git@shurik.me>
2025-10-04 01:25:09 +0300

    Only cache version on success

--- update-metadata.sh
+++ update-metadata.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 
+GAME_INSTALL_DIR='/tmp/rocketleague'
+
 # Install scripts requirements
 pip install -r requirements.txt
 chmod +x calculate_build_id.py convert_to_json.py
 
 # Rocket League data
-strings /tmp/rocketleague/Binaries/Win64/RocketLeague.exe > rocketleague.txt
-strings -e l /tmp/rocketleague/Binaries/Win64/RocketLeague.exe > rocketleague-16.txt
+strings "$GAME_INSTALL_DIR/Binaries/Win64/RocketLeague.exe" > rocketleague.txt
+strings -e l "$GAME_INSTALL_DIR/Binaries/Win64/RocketLeague.exe" > rocketleague-16.txt
 
 function get_feature_set() {
     local FEATURE_SET=$(cat rocketleague-16.txt | grep -E 'Update[0-9]+' | uniq -d | head -1)
@@ -33,8 +35,8 @@ echo "psy_build_id=$PSY_BUILD_ID" >> rocket_league_info
 echo "feature_set=$FEATURE_SET" >> rocket_league_info
 
 # EOS SDK data
-strings rocketleague/Binaries/Win64/EOSSDK-Win64-Shipping.dll > eossdk.txt
-strings -e l rocketleague/Binaries/Win64/EOSSDK-Win64-Shipping.dll > eossdk-16.txt
+strings "$GAME_INSTALL_DIR/Binaries/Win64/EOSSDK-Win64-Shipping.dll" > eossdk.txt
+strings -e l "$GAME_INSTALL_DIR/Binaries/Win64/EOSSDK-Win64-Shipping.dll" > eossdk-16.txt
 
 EOSSDK_VERSION=$(cat eossdk-16.txt | grep -E '^([0-9]+\.){2,}[0-9]-[a-Z0-9]+' | head -1)
 
@@ -50,7 +52,7 @@ mkdir -p data
 
 ./legendary list-files Sugar > data/files.txt 2> /dev/null
 
-mv /tmp/rocketleague/Legal/PC/* data/legal/
+mv "$GAME_INSTALL_DIR/Legal/PC/"* data/legal/
 mv "$LEGENDARY_CONFIG_PATH/manifests/"Sugar_Windows_*.manifest data/Sugar.manifest
 
 echo "build_id=$G_PSYONIX_BUILD_ID" >> $GITHUB_OUTPUT
