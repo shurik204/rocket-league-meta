92a260f7cee8130761badb1de081068dfc3f9085
shurik204 <git@shurik.me>
2025-10-04 16:54:17 +0300

    More error handling, added game curl version, removed eos_sdk.json

--- update-metadata.sh
+++ update-metadata.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -e
+set -Eeuo pipefail
 set -x
 
 grep --version | head -1
 if [ -z "$GAME_INSTALL_DIR" ]; then
     GAME_INSTALL_DIR='/tmp/rocketleague'
 fi
+echo "Game install directory: '$GAME_INSTALL_DIR'"
 
 # Install scripts requirements
 pip install -r requirements.txt
@@ -42,37 +43,35 @@ fi
 PSY_BUILD_ID=$(./calculate_build_id.py "$G_PSYONIX_BUILD_ID")
 FEATURE_SET=$(get_feature_set)
 BUILD_DATE=$(cat rocketleague.txt | grep -E '[A-Z][a-z]{2,8} {1,3}[0-9]+ 20[0-9]+ [0-9]+:[0-9]+:[0-9]+' | head -1)
+CURL_VERSION=$(cat rocketleague.txt | grep -E '^libcurl/([0-9]+\.){1,3}[0-9]+' | head -1 | cut -b 9-)
 
 echo "build_date=$BUILD_DATE" >> rocket_league_info
 echo "g_psyonix_build_id=$G_PSYONIX_BUILD_ID" >> rocket_league_info
-echo "psy_build_id=$PSY_BUILD_ID" >> rocket_league_info
+echo "build_id=$PSY_BUILD_ID" >> rocket_league_info
 echo "feature_set=$FEATURE_SET" >> rocket_league_info
-
+echo "curl_version=$CURL_VERSION" >> rocket_league_info
 # EOS SDK data
 echo "Extracting EOS SDK data"
 
 strings "$GAME_INSTALL_DIR/Binaries/Win64/EOSSDK-Win64-Shipping.dll" > eossdk.txt
 strings -e l "$GAME_INSTALL_DIR/Binaries/Win64/EOSSDK-Win64-Shipping.dll" > eossdk-16.txt
 
-rm -f eos_sdk_info 2> /dev/null
-
 EOSSDK_VERSION=$(cat eossdk-16.txt | grep -E '^([0-9]+\.){2,}[0-9]-[A-z0-9]+' | head -1)
 
-echo "eos_sdk_version=$EOSSDK_VERSION" >> eos_sdk_info
+echo "eos_sdk_version=$EOSSDK_VERSION" >> rocket_league_info
 
 # Collect results
 echo "Collecting results"
 mkdir -p data
 
-./convert_to_json.py rocket_league_info data/rocket_league.json
-./convert_to_json.py eos_sdk_info data/eos_sdk.json
+./convert_to_json.py rocket_league_info data/Sugar.json
 
 echo "Writing list of game files"
 legendary list-files Sugar > data/files.txt 2> /dev/null
 
 echo "Storing legal texts"
-mkdir -p data/legal
-mv "$GAME_INSTALL_DIR/TAGame/Legal/PC/"* data/legal/
+mkdir -p data/Legal
+mv "$GAME_INSTALL_DIR/TAGame/Legal/PC/"* data/Legal/
 echo "Storing game manifest"
 mv "$LEGENDARY_CONFIG_PATH/manifests/"Sugar_Windows_*.manifest data/Sugar.manifest
 
